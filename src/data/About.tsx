8adfa3ecf9ec9d0c4754273a81632a8c105fd989
cuddlemonstah <killingspreeisthegreat99@gmail.com>
2025-05-08 22:57:35 +0800

    About plus upload

--- src/data/About.tsx
+++ src/data/About.tsx
@@ -5,5 +5,5 @@ interface AboutType {
 
 export const About: AboutType = {
   about1: "Grade and classify fruits with AI",
-  about2: "Get started with your photos or use real-time camera",
+  about2: "Get started with your photos of fruits",
 };
